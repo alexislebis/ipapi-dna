8414ac4ad326ae49cf14bc11e64a4b7d1150763d
Alexis <alexis@Sae>
2022-10-27 15:47:01 +0200

    init repo + q14 wrong implem

---
+++ q14/q-implem/initial-code.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def reconstruct(arnSeq:str, prots:List[str]):
+    for prot in prots:
+        arnSeq += mostProbableNucl(prot) #What about overlaping prot and triplets?
+    
+    #TODO:arnSeq no guarantee to have either U{A,G,C}A!
+    
+    return arnSeq
